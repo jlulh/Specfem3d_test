91587ced7899fe7033df3a9b4c9895461bdfb1cd
jlulh <177344303@qq.com>
2024-10-31 16:11:46 +0800

    first commit

---
+++ specfem3d-master/.github/scripts/run_build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# builds all executables
+#
+
+# getting updated environment (CUDA_HOME, PATH, ..)
+if [ -f $HOME/.tmprc ]; then source $HOME/.tmprc; fi
+
+WORKDIR=`pwd`
+
+# info
+echo "work directory: $WORKDIR"
+echo `date`
+echo
+echo "**********************************************************"
+echo
+echo "configuration test: TESTFLAGS=${TESTFLAGS} TESTNGLL=${TESTNGLL}"
+echo
+echo "**********************************************************"
+echo
+
+# compiler infos
+echo "compiler versions:"
+echo "gcc --version"
+gcc --version
+echo "gfortran --version"
+gfortran --version
+echo "mpif90 --version"
+mpif90 --version
+echo
+
+## ADIOS2
+if [ "${ADIOS2}" == "true" ]; then
+  echo
+  echo "enabling ADIOS2"
+  echo
+  ADIOS2_CONFIG="${ADIOS2_DIR}/bin/adios2-config"
+  adios=(--with-adios2 ADIOS2_CONFIG="$ADIOS2_CONFIG" )
+else
+  adios=()
+fi
+
+# configuration
+echo
+echo "configuration:"
+echo
+
+./configure \
+${adios[@]} \
+FC=gfortran MPIFC=mpif90 CC=gcc ${TESTFLAGS}
+
+# checks
+if [[ $? -ne 0 ]]; then echo "configuration failed:"; cat config.log; echo ""; echo "exiting..."; exit 1; fi
+
+# layered example w/ NGLL = 6
+if [ "$TESTNGLL" == "6" ]; then
+  sed -i "s:NGLLX =.*:NGLLX = 6:" setup/constants.h
+fi
+
+# we output to console
+sed -i "s:IMAIN .*:IMAIN = ISTANDARD_OUTPUT:" setup/constants.h
+
+# compilation
+echo
+echo "compilation:"
+make clean; make -j2 all
+
+# checks
+if [[ $? -ne 0 ]]; then exit 1; fi
+
+echo
+echo "done "
+echo `date`
+echo
